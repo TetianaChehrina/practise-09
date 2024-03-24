bfab43798210ca9b5bcf7dd8ffb2009ee814e482
Oleksandr Pluzhnykov <alexanderp@devart.com>
2024-03-24 16:55:55 +0200

    finish task 5

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -1,13 +1,39 @@
 export class UnsplashAPI {
   #BASE_URL = 'https://api.unsplash.com/search/photos';
   #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
+  #query = '';
+  #searchParams = new URLSearchParams({
+    per_page: 12,
+    orientation: 'portrait',
+    client_id: this.#API_KEY
+  })
+
+  set query(newQuery){
+    this.#query = newQuery;
+  }
 
   getPopularPhotos(page) {
-    const url = `${
-      this.#BASE_URL
-    }?page=${page}&query=popular&per_page=12&orientation=portrait&client_id=${
-      this.#API_KEY
-    }`;
+    // const url = `${
+    //   this.#BASE_URL
+    // }?page=${page}&query=popular&per_page=12&orientation=portrait&client_id=${
+    //   this.#API_KEY
+    // }`;
+    const url = `${this.#BASE_URL}?page=${page}&query=popular&${this.#searchParams}`
+    return fetch(url).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
+  }
+
+  getImagesByQuery(page) {
+    // const url = `${
+    //   this.#BASE_URL
+    // }?page=${page}&query=${this.#query}&per_page=12&orientation=portrait&client_id=${
+    //   this.#API_KEY
+    // }`;
+    const url = `${this.#BASE_URL}?page=${page}&query=${this.#query}&${this.#searchParams}`
     return fetch(url).then(response => {
       if (!response.ok) {
         throw new Error(response.status);
